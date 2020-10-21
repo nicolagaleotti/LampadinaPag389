a469538bc0cc1b446fe289c9d55c675549e57707
Nicola Galeotti <niccio2802@outlook.it>
2020-10-21 12:52:59 +0200

    Add project files.

---
+++ LaSfidaPag389/Lampadina.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LaSfidaPag389
+{
+    public class Lampadina
+    {
+        private string stato;
+        private const int numeroClickMassimi = 5;
+        private int contatore = 0;
+        public Lampadina(string stato)
+        {
+            this.stato = stato;
+        }
+
+        public string Descrizione()
+        {
+            return stato;
+        }
+
+        public void Click()
+        {
+            if (stato != "rotta")
+            {
+                contatore++;
+                if (contatore <= numeroClickMassimi)
+                {
+                    if (stato == "accesa")
+                        stato = "spenta";
+                    else if (stato == "spenta")
+                        stato = "accesa";
+                }
+                else stato = "rotta";
+            }
+        }
+    }
+}
---
+++ Prova/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using LaSfidaPag389;
+
+namespace Prova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lampadina l1 = new Lampadina("accesa");
+            for (int i = 0; i <=10; i++)
+            {
+                l1.Click();
+                Console.WriteLine($"Lampadina {l1.Descrizione()}.");
+            }
+        }
+    }
+}
